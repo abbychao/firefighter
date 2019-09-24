a0f4cf8132ea55c5f37d802759d5ffee2dd4491f
Abby Chao <abigail.chao@gmail.com>
2019-09-23 20:56:03 -0700

    Created more space for admin styling

--- client/components/admin/Admin.js
+++ client/components/admin/Admin.js
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
 import List from './List';
 import Form from './Form';
 import AdminContext from './Context';
+
+const StyledAdmin = styled.div`
+  width: 1000px;
+`;
 
 const Admin = () => {
   const context = useContext(AdminContext);
   const { showForm, currentQ } = context;
 
   return (
-    <>
+    <StyledAdmin>
       <Nav />
       <List />
       {showForm && <Form currentQ={currentQ} />}
-    </>
+    </StyledAdmin>
   );
 }
 
