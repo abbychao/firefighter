1617da45d4b56fd2247cbbb3e3bab41c03336bb2
Abby Chao <abigail.chao@gmail.com>
2019-12-18 15:50:33 -0800

    Clean up

--- database/questionSchema.js
+++ database/questionSchema.js
@@ -1,10 +1,7 @@
 const { Schema } = require('mongoose');
 
 const questionSchema = new Schema({
-  // id: { type: Number, unique: true, required: true },
   scenario: { type: String, required: true },
-  // buildingType: { type: String, required: true },
-  // fireType: { type: String, required: true },
   question: { type: String, required: true },
   options: { type: Object, required: true },
   answerIndex: { type: Number, required: true },
