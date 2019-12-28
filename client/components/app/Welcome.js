a7281b73a7182f93c23244e020c19c033215c6ba
Abby Chao <abigail.chao@gmail.com>
2019-12-28 14:24:24 -0800

    Created dropdowns

--- client/components/app/Welcome.js
+++ client/components/app/Welcome.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppContext from './Context';
 
 const Welcome = () => {
@@ -6,9 +6,34 @@ const Welcome = () => {
   const { scenarios, getScenarios } = context;
   useEffect(getScenarios, []);
 
+  // Options are populated into the dropdowns
+  const [buildingOptions, setBuildingOptions] = useState('');
+  const [buildingDetailsOptions, setBuildingDetailsOptions] = useState('');
+  const [positionOptions, setPositionOptions] = useState('');
+  const [DueOptions, setDueOptions] = useState('');
+
+  // Selected elements are the current selection
+  const [buildingSelected, setBuildingSelected] = useState('');
+  const [buildingDetailsSelected, setBuildingDetailsSelected] = useState('');
+  const [positionSelected, setPositionSelected] = useState('');
+  const [dueSelected, setDueSelected] = useState('');
+
   const scenariosHTML = [];
+  const buildingSet = new Set(['Building Type']);
+  const buildingDetailsSet = new Set(['Details']);
+  const positionSet = new Set(['Position']);
+  const dueSet = new Set(['Due']);
   scenarios.forEach((scenario) => {
-    const { building, buildingDetails, position, due } = scenario;
+    const {
+      building,
+      buildingDetails,
+      position,
+      due,
+    } = scenario;
+    buildingSet.add(building);
+    buildingDetailsSet.add(buildingDetails);
+    positionSet.add(position);
+    dueSet.add(due);
     const name = `${building} ${buildingDetails} ${position} ${due} Due`;
     scenariosHTML.push(
       <button
@@ -21,6 +46,16 @@ const Welcome = () => {
       </button>,
     );
   });
+  const getOptionHTML = (set) => [...set].map((item) => (
+    <option key={`option-${item}`}>
+      {item}
+    </option>
+  ));
+
+  const buildingHTML = getOptionHTML(buildingSet);
+  const buildingDetailsHTML = getOptionHTML(buildingDetailsSet);
+  const positionHTML = getOptionHTML(positionSet);
+  const dueHTML = getOptionHTML(dueSet);
 
   return (
     <>
@@ -28,6 +63,19 @@ const Welcome = () => {
       <h1>Welcome!</h1>
       <p>Get ready to ace the Lieutenant's exam! This simulation will help you prepare.</p>
       <p>What is your scenario?</p>
+      <select onChange={(e) => console.log(e.target.value)}>
+        {buildingHTML}
+      </select>
+      <select>
+        {buildingDetailsHTML}
+      </select>
+      <select>
+        {positionHTML}
+      </select>
+      <select>
+        {dueHTML}
+      </select>
+      <br />
       {scenariosHTML}
     </ >
   );
