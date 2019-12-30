043fa4c1f20f730a1be525502a7f1219dc32ca94
Abby Chao <abigail.chao@gmail.com>
2019-12-30 11:50:01 -0800

    Updated Nav styling

--- client/components/app/Nav.js
+++ client/components/app/Nav.js
@@ -3,13 +3,25 @@ import AppContext from './Context'
 
 const Nav = () => {
   const context = useContext(AppContext);
-  const { questions, questionIndex } = context;
+  const { currentScenario } = context;
 
   return (
     <section id="nav">
       <div>
-        Scenario:
-        <label> {questions[questionIndex].scenario}</label>
+        Building:
+        <span className="bold">{currentScenario.building}</span>
+      </div>
+      <div>
+        Details:
+        <span className="bold">{currentScenario.buildingDetails}</span>
+      </div>
+      <div>
+        Position:
+        <span className="bold">{currentScenario.position}</span>
+      </div>
+      <div>
+        Due:
+        <span className="bold">{currentScenario.due}</span>
       </div>
       <button onClick={() => context.setView('admin')}>Switch to Admin</button>
     </section >
